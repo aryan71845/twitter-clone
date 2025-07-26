ce99600e4dd39932d28150c6b959ca37cdd5d4e1
aryan71845 <aryan.ishu007@gmail.com>
2025-07-27 01:04:59 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js"
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+
 const app= express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // for raw JSON
+app.use(express.urlencoded({ extended: true })); // for x-www-form-urlencoded
+
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 
