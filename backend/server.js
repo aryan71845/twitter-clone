aceb8d29946bbe842cc42e8bbba4c2fc916e9046
aryan71845 <aryan.ishu007@gmail.com>
2025-07-28 17:56:38 +0530

    1-user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js"
+import userRoutes from "./routes/user.routes.js"
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
 dotenv.config();
 
-const app= express();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // for raw JSON
@@ -15,9 +23,10 @@ app.use(express.urlencoded({ extended: true })); // for x-www-form-urlencoded
 
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 });
