9cf8f24f7efb4138c11e9a9d9869d783b1a1220c
aryan71845 <aryan.ishu007@gmail.com>
2025-07-29 20:15:22 +0530

    1-notification-routesand controllers added

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 
-import authRoutes from "./routes/auth.route.js"
-import userRoutes from "./routes/user.route.js"
-import postRoutes from "./routes/post.route.js"
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
